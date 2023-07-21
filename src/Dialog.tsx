ddc2d400aff91baae1c927dfc90c24d9126a18b5
Varun Potti <potti.varun07@gmail.com>
2023-07-21 18:19:09 +0530

    fix: dialog styles

--- src/Dialog.tsx
+++ src/Dialog.tsx
@@ -1,6 +1,7 @@
 "use client";
 import * as DialogPrimitives from "@radix-ui/react-dialog";
 import { X } from "phosphor-react";
+import { Button } from "..";
 
 interface Props {
   children: React.ReactNode[];
@@ -18,20 +19,46 @@ const Dialog = (props: Props) => (
       {props.children[0]}
     </DialogPrimitives.Trigger>
     <DialogPrimitives.Portal>
-      <DialogPrimitives.Overlay className="bg-black/20 data-[state=open]:animate-overlayShow fixed inset-0" />
-      <DialogPrimitives.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white-fill p-5 shadow-[0px_0px_5px_0px] shadow-white-dropshadow focus:outline-none">
+      <DialogPrimitives.Overlay className="bg-black/10 data-[state=open]:animate-overlayShow fixed inset-0" />
+      <DialogPrimitives.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white-fill p-5 shadow-[0px_2px_0px_0px] shadow-white-dropshadow focus:outline-none">
         {props.children[1]}
         <DialogPrimitives.Close asChild>
-          <button
-            className="text-white-text hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+          <Button
+            variant={"secondary"}
+            id="dialog-close"
+            className="text-black p-2 absolute top-3 right-3"
             aria-label="Close"
           >
             <X />
-          </button>
+          </Button>
         </DialogPrimitives.Close>
       </DialogPrimitives.Content>
     </DialogPrimitives.Portal>
   </DialogPrimitives.Root>
 );
+
+export const DialogButtons = ({
+  onClick,
+  confirmationText,
+}: {
+  onClick?: () => void;
+  confirmationText?: string;
+}) => {
+  return (
+    <div className="flex gap-2 items-center justify-end mt-2">
+      <Button
+        variant="secondary"
+        onClick={() => {
+          document.getElementById("dialog-close")?.click();
+        }}
+      >
+        Cancel
+      </Button>
+      <Button variant="primary" onClick={onClick}>
+        {confirmationText || "Confirm"}
+      </Button>
+    </div>
+  );
+};
 
 export { Dialog };
